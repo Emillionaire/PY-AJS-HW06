a927d195f184b81a842f2d7bf69ba092280293ce
Rovnin Nikita <Nikita.Rovnin@gmail.com>
2023-04-17 12:03:03 +0700

    Rebuild

--- task_2/src/index.js
+++ task_2/src/index.js
@@ -21,28 +21,84 @@ const character = {
     ]	
 };
 
-function destructor(attackId) {
-    let specialAttacks = character.special;
+const character2 = {
+    name: 'Воин',
+    type: 'Bowman',
+    special: [
+        {
+            id: 1,
+            name: 'Рубить',
+            icon: 'http://...',
+            // description: 'Разрубает пополам',
+        },
+        {
+            id: 2,
+            name: 'Крушить',
+            icon: 'http://...',
+            description: 'Крушит!',
+        },
+        {
+            id: 3,
+            name: 'Ломать',
+            icon: 'http://...',
+            // description: 'Ломает',
+        },
+    ]
+}
 
-    result = {};
+function destructor(char) {
+    let attacks = char.special;
 
-    let res = specialAttacks.some(attr => attr.id == attackId);
+    result = [];
 
-    return specialAttacks.find(element => {
-        if (element.id == attackId) {
-            let {id, name, icon, description} = element;
+    attacks.forEach(element => {
 
-            if (description == undefined) {
-                element.description = 'Описание недоступно!';
-            }
-
-            return element
+        if (element.description == undefined) {
+            element.description = 'Описание недоступно!';
         }
+
+        let {id, name, icon, description} = element;
+
+        result.push({
+            'id': id,
+            'name': name,
+            'icon': icon,
+            'description': description,
+        });
     })
-    
+
+    return result;
 }
 
 
 // Calling function
-console.log(destructor(8));
-console.log(destructor(9));
+console.log(destructor(character));
+console.log(destructor(character2));
+
+
+
+// function destructor(attackId) {
+//     let specialAttacks = character.special;
+
+//     result = {};
+
+//     let res = specialAttacks.some(attr => attr.id == attackId);
+
+//     return specialAttacks.find(element => {
+//         if (element.id == attackId) {
+//             let {id, name, icon, description} = element;
+
+//             if (description == undefined) {
+//                 element.description = 'Описание недоступно!';
+//             }
+
+//             return element
+//         }
+//     })
+    
+// }
+
+
+// // Calling function
+// console.log(destructor(8));
+// console.log(destructor(9));
